1a6ceb323f837130880d22f6ad4fa93d24ef10e8
Abdelghaffar Nasser <58223290+AbdelghaffarNasser@users.noreply.github.com>
2021-06-05 10:16:23 +0200

    mainFile

---
+++ main.java
@@ -0,0 +1,15 @@
+package project;
+
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class main extends JTextPane {
+
+
+    public final static String NAME = "Editor";
+    
+    public static void main(String[] args) {
+        new userInterface().setVisible(true);
+    }
+
+}
