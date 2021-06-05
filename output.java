31a5f116ff9d990f253356822a8ff00ec9879bea
Abdelghaffar Nasser <58223290+AbdelghaffarNasser@users.noreply.github.com>
2021-06-05 10:16:42 +0200

    outputFile

---
+++ output.java
@@ -0,0 +1,39 @@
+package project;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class output {
+
+    public static void show(ArrayList<lexicalAnalyzer.Token> tokens) {
+        String[] Column = {"Line No", "Lexeme", "Return Token", "Lexeme No in Line", "Matchability"};
+        JLabel label = new JLabel("Total NO of errors: " + lexicalAnalyzer.counter);
+
+        DefaultTableModel model = new DefaultTableModel(Column, 0);
+
+        for (lexicalAnalyzer.Token token : tokens)
+            model.addRow(new Object[]{token.line, token.lexeme, token.tokentype, token.pos, "Matched"});
+
+
+        JTable table = new JTable(model) {
+            public boolean isCellEditable(int r, int w) {
+                return false;
+            }
+        };
+
+
+        Font font = new Font("Verdana", Font.PLAIN, 12);
+        table.setFont(font);
+        table.setRowHeight(30);
+        JFrame frame = new JFrame();
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500, 500);
+        frame.add(new JScrollPane(table));
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLayout(new FlowLayout());
+        frame.add(label);
+    }
+}
